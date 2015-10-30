b54e79bc62515ce0664abad2b7b3c02499ee051c
Mark Fine <mark.fine@gmail.com>
2015-10-30 13:42:08 -0700

    Fix up the ddb typeclass.

--- src/Network/Skylark/Core/Update.hs
+++ src/Network/Skylark/Core/Update.hs
@@ -7,7 +7,14 @@
 -- Update module for Skylark Core.
 
 module Network.Skylark.Core.Update
-  ( Update (..)
+  ( Update
+  , open
+  , close
+  , key
+  , openExprs
+  , closeExprs
+  , openValues
+  , closeValues
   ) where
 
 import           BasicPrelude
@@ -21,60 +28,52 @@ import           Network.Skylark.Core.Types
 type AttributeValueMap = HashMap Text AttributeValue
 
 class Update a where
+  update :: AWSConstraint e m => Text -> Text -> [Text] -> AttributeValueMap -> a -> m ()
+  update table expr exprs values item = do
+    t <- liftIO $ timestamp
+    void $ send $
+      updateItem table &
+        uiKey .~ key item &
+        uiConditionExpression .~ Just expr &
+        uiUpdateExpression .~ Just updateExpr &
+        uiExpressionAttributeValues .~ updateValues t where
+          updateExpr = "SET " <> intercalate ", " exprs
+          updateValues t = values <> M.fromList
+            [ (":t", attributeValue & avS .~ Just t)
+            ]
+
+  open :: AWSConstraint e m => Text -> a -> m ()
+  open table item = update table expr exprs values item where
+    expr = "attribute_not_exists(updated_at) OR updated_at <= :t"
+    exprs = openExprs item <>
+      [ "opened_at  = if_not_exists(opened_at, :t)"
+      , "updated_at = if_not_exists(closed_at, :t)"
+      ]
+    values = openValues item
+
+  close :: AWSConstraint e m => Text -> a -> m ()
+  close table item = update table expr exprs values item where
+    expr = "attribute_not_exists(updated_at) OR updated_at <= :t"
+    exprs = closeExprs item <>
+      [ "opened_at  = if_not_exists(opened_at, :t)"
+      , "updated_at = if_not_exists(closed_at, :t)"
+      , "closed_at  = if_not_exists(closed_at, :t)"
+      ]
+    values = closeValues item
+
   key :: a -> AttributeValueMap
+
+  openExprs :: a -> [Text]
+  openExprs _item = mempty
+
+  closeExprs :: a -> [Text]
+  closeExprs _item = mempty
 
   openValues :: a -> AttributeValueMap
   openValues _item = M.empty
 
   closeValues :: a -> AttributeValueMap
   closeValues _item = M.empty
-
-  openUpdateExprs :: a -> [Text]
-  openUpdateExprs _item = mempty
-
-  closeUpdateExprs :: a -> [Text]
-  closeUpdateExprs _item = mempty
-
-  openItem :: AWSConstraint e m => Text -> a -> m ()
-  openItem table item = do
-    t <- liftIO $ timestamp
-    void $ send $
-      updateItem table &
-        uiKey .~ key item &
-        uiExpressionAttributeValues .~ values t &
-        uiConditionExpression .~ Just conditionExpr &
-        uiUpdateExpression .~ Just updateExpr where
-          values t = openValues item <>
-            M.fromList
-              [ (":t", attributeValue & avS .~ Just t)
-              ]
-          conditionExpr = "attribute_not_exists(updated_at) OR updated_at <= :t"
-          updateExpr = "SET " <> intercalate ", " updateExprs
-          updateExprs = openUpdateExprs item <>
-            [ "opened_at  = if_not_exists(opened_at, :t)"
-            , "updated_at = if_not_exists(closed_at, :t)"
-            ]
-
-  closeItem :: AWSConstraint e m => Text -> a -> m ()
-  closeItem table item = do
-    t <- liftIO $ timestamp
-    void $ send $
-      updateItem table &
-        uiKey .~ key item &
-        uiExpressionAttributeValues .~ values t &
-        uiConditionExpression .~ Just conditionExpr &
-        uiUpdateExpression .~ Just updateExpr where
-          values t = closeValues item <>
-            M.fromList
-              [ (":t", attributeValue & avS .~ Just t)
-              ]
-          conditionExpr = "attribute_not_exists(updated_at) OR updated_at <= :t"
-          updateExpr = "SET " <> intercalate ", " updateExprs
-          updateExprs = closeUpdateExprs item <>
-            [ "opened_at  = if_not_exists(opened_at, :t)"
-            , "updated_at = if_not_exists(closed_at, :t)"
-            , "closed_at  = if_not_exists(closed_at, :t)"
-            ]
 
 timestamp :: IO Text
 timestamp = do

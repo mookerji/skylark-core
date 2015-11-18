0f333e6d933507b4523edc7ecd7ce881b948f5ca
Mark Fine <mark.fine@gmail.com>
2015-11-17 16:29:30 -0800

    Add a Receipts module.

---
+++ src/Network/Skylark/Core/Receipts.hs
@@ -0,0 +1,19 @@
+-- |
+-- Module:      Network.Skylark.Core.Receipts
+-- Copyright:   (c) 2015 Mark Fine
+-- License:     BSD3
+-- Maintainer:  Mark Fine <mark@swift-nav.com>
+--
+-- Receipts module for Skylark Core.
+
+module Network.Skylark.Core.Receipts
+  ( generate
+  ) where
+
+import Data.UUID.V4
+import Data.Time.Clock
+import Network.Skylark.Core.Prelude
+import Network.Skylark.Core.Types
+
+generate :: IO Receipt
+generate = Receipt <$> nextRandom <*> getCurrentTime

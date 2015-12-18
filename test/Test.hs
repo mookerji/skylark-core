eb07895dd0483dd55dbe89ad478f694f82afb3bc
Mark Fine <mark.fine@gmail.com>
2015-12-17 22:29:02 -0800

    Nits on PR.

--- test/Test.hs
+++ test/Test.hs
@@ -7,12 +7,11 @@
 -- Test module for Skylark Core.
 
 import           BasicPrelude
-import qualified Test.Network.Skylark.Core.Config as Config
 import           Test.Tasty
 
 tests :: TestTree
 tests = testGroup "Tests"
-  [ Config.tests
+  [
   ]
 
 main :: IO ()

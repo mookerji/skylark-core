0b300aff0efed6479bb58987fffcafc51d555e1a
Mark Fine <mark.fine@gmail.com>
2015-12-22 23:54:52 -0500

    Lift more conf and setup to core.

---
+++ src/Network/Skylark/Core/Constants.hs
@@ -0,0 +1,21 @@
+-- |
+-- Module:      Network.Skylark.Core.Constants
+-- Copyright:   (c) 2015 Mark Fine
+-- License:     BSD3
+-- Maintainer:  Mark Fine <mark@swift-nav.com>
+--
+-- Constants module for Skylark Core.
+
+module Network.Skylark.Core.Constants where
+
+import Network.Skylark.Core.Prelude
+
+awsAccessKey :: Text
+awsAccessKey = "AWS_ACCESS_KEY_ID"
+
+awsSecretKey :: Text
+awsSecretKey = "AWS_SECRET_ACCESS_KEY"
+
+jitterRate :: Double
+jitterRate = 0.15
+
--- src/Network/Skylark/Core/Timers.hs
+++ src/Network/Skylark/Core/Timers.hs
@@ -10,23 +10,21 @@ module Network.Skylark.Core.Timers
   ( whenExpires
   ) where
 
-import Control.Lens
 import Control.Monad.Random
 import Data.IORef
 import Data.Time
+import Network.Skylark.Core.Constants
 import Network.Skylark.Core.Prelude
-import Network.Skylark.Core.Types
 
 -- | Jitter value by a random factor.
-jitter :: MonadRandom m => Double -> Double -> m Double
-jitter v r = (* v) <$> getRandomR (1, 1 + r)
+jitter :: MonadRandom m => Double -> m Double
+jitter v = (* v) <$> getRandomR (1, 1 + jitterRate)
 
 -- | Check timer and update if elapsed, run action if expired.
-whenExpires :: MonadCore e m => UTCTime -> IORef UTCTime -> Double -> m () -> m ()
+whenExpires :: (MonadRandom m, MonadIO m) => UTCTime -> IORef UTCTime -> Double -> m () -> m ()
 whenExpires time expires interval a = do
   expires' <- liftIO $ readIORef expires
   when (time >= expires') $ do
-    rate <- view ctxJitterRate
-    interval' <- jitter interval rate
+    interval' <- jitter interval
     liftIO $ writeIORef expires $ addUTCTime (fromRational $ toRational interval') time
     a

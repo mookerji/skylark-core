bc694a85db077f5a59770bfafef0e0c2f4f7d0ab
Mark Fine <mark.fine@gmail.com>
2016-01-14 18:02:58 -0800

    Expose runCoreIO

--- src/Network/Skylark/Core.hs
+++ src/Network/Skylark/Core.hs
@@ -10,14 +10,20 @@ module Network.Skylark.Core
   ( module Network.Skylark.Core.Types
   , runResourceT
   , runCoreT
+  , runCoreIO
   ) where
 
 import Control.Lens
 import Control.Monad.Logger
 import Control.Monad.Trans.AWS
+import Control.Monad.Trans.Resource
 import Network.Skylark.Core.Prelude
 import Network.Skylark.Core.Types
 
-runCoreT :: HasCtx r => r -> CoreT r m a -> m a
+runCoreT :: HasCtx e => e -> CoreT e m a -> m a
 runCoreT e (CoreT m) =
   runAWST e $ runLoggingT m $ e ^. ctxLog
+
+runCoreIO :: HasCtx e => e -> CoreT e (ResourceT IO) a -> IO a
+runCoreIO e a =
+  runResourceT $ runCoreT e a

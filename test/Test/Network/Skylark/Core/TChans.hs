48f5a34e0cde7bf3e64c60c3af63923677149454
Mark Fine <mark.fine@gmail.com>
2016-02-05 15:44:12 -0800

    First properties.

---
+++ test/Test/Network/Skylark/Core/TChans.hs
@@ -0,0 +1,13 @@
+-- |
+-- Module:      Test.Network.Skylark.Core.TChans
+-- Copyright:   (c) 2015 Mark Fine
+-- License:     BSD3
+-- Maintainer:  Mark Fine <mark@swift-nav.com>
+--
+-- Test send and receive TChans module for Skylark Core.
+
+module Test.Network.Skylark.Core.Retries
+  ( tests
+  ) where
+
+

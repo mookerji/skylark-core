cb5b8eb096b296cdf3b0e266da04aa62ce87b248
Mark Fine <mark.fine@gmail.com>
2016-02-15 21:29:01 -0800

    Move to a makefile.

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo "Building Skylark Core"
-stack build skylark-core "$@"
-
--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo "Testing Skylark Core library"
-AWS_SECRET_ACCESS_KEY=foo AWS_ACCESS_KEY_ID=bar stack build skylark-core --test --coverage "$@"
